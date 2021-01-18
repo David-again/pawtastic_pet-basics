6840333434b8a0a34f047c7e17776db79b0a64ac
David-again <d.ogunbanjo@yahoo.com>
2021-01-17 19:41:23 -0500

    fixed broken javascript for mobile-menu

--- src/scripts/Modules/MobileMenu_ES6.js
+++ src/scripts/Modules/MobileMenu_ES6.js
@@ -6,13 +6,34 @@ class MobileMenu_ES6 {
     }
 
     events() {
-        this.menuIcon.addEventListener("click", () => this.toggleTheMenu());
+        // this.menuIcon.addEventListener("click", () => this.toggleTheMenu());
+        this.menuIcon.addEventListener("click", () => this.triggerTheMenu());
     }
 
-    toggleTheMenu() {
-        console.log("The Menu Icon was clicked!");
-        this.menuContainer.classList.toggle("navbar__menu-container--is-visible");
-        this.menuIcon.classList.toggle("navbar__menu-icon--close-x")
+    // toggleTheMenu() {
+    //     console.log("The Menu Icon was clicked!");
+    //     this.menuContainer.classList.toggle("navbar__menu-container--is-visible");
+    //     this.menuIcon.classList.toggle("navbar__menu-icon--close-x")
+    // }
+
+    triggerTheMenu() {
+        if (this.menuContainer.classList.contains("navbar__menu-container--is-visible")) {
+            this.closeTheMenu();
+        } else {
+            this.openTheMenu();
+        }
+    }
+
+    closeTheMenu() {
+        console.log("Menu-close icon was clicked!");
+        this.menuContainer.classList.remove("navbar__menu-container--is-visible");
+        this.menuIcon.classList.remove("navbar__menu-icon--close-x");
+    }
+
+    openTheMenu() {
+        console.log("The Menu was clicked-open!");
+        this.menuContainer.classList.add("navbar__menu-container--is-visible");
+        this.menuIcon.classList.add("navbar__menu-icon--close-x");
     }
 }
 
